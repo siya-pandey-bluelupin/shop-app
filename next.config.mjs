43ac9caf6b9bd9297cb942ac39201acb12e77251
siya-pandey-bluelupin <siya@bluelupin.com>
2024-11-26 18:02:42 +0530

    complete micro frontend

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,30 @@
-/** @type {import('next').NextConfig} */
+import NextFederationPlugin from '@module-federation/nextjs-mf';
+
 const nextConfig = {
-  reactStrictMode: true,
+  webpack(config, options) {
+    const { isServer } = options;
+    config.plugins.push(
+      new NextFederationPlugin({
+        name: 'shop_app',
+        remotes: {},
+        filename: 'static/chunks/remoteEntry.js',
+        exposes: {
+          './Products': './components/Products.js',
+          './category': './pages/category/index.js',
+          './books': './pages/books/index.js',
+
+
+        },
+        extraOptions: {
+          debug: false, // `false` by default
+          exposePages: false, // `false` by default
+        },
+        shared: {},
+      })
+    );
+
+    return config;
+  },
 };
 
 export default nextConfig;
